8f0eb7fa91286170a17ad33b3ecac4a4689f6346
thainarajanuario <gigitata199427@gmail.com>
2022-09-29 10:47:40 -0300

    Exercicios-Collection

---
+++ collections.ts
@@ -0,0 +1,46 @@
+//Três formas de iniciar uma Array
+
+//let identificador = new Array<string>
+//let identificador: string[] = []
+//let identificador = ["nome", "Giovana"]
+//console.log(identificador[0]);
+
+
+//let alunos: string[] = []
+
+//alunos.push('Debora', 'Romulo', 'Mayara', 'Sergio');
+
+
+//console.log(alunos[2][3]);
+
+
+//for(let i = 0; i <= 2; i++){
+//    console.log(alunos[i])
+//}
+
+//for( let i of alunos){
+//    console.log(i)
+//}
+
+//alunos.forEach(abacaxi => console.log(abacaxi))
+
+
+//exercicio1
+
+/*let estoque: string[] = []
+
+estoque.push('blusa', 'calça', 'short', 'saia')
+console.log(estoque);
+
+estoque.pop();
+console.log(estoque);
+
+estoque.splice(3,3, "boné");
+console.log(estoque);*/
+
+
+//exercicio2
+
+/*let Imc = (Peso: number, Altura: number) => (Peso/ (Altura * Altura)).toFixed(2);
+console.log(Imc (77,1.77));*/
+
